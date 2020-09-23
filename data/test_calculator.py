8413c3523164b4640e560e4ab62f1f6a4dc556f4
vatican1 <33574109+vatican1@users.noreply.github.com>
2020-09-23 10:02:43 +0300

    Добавлены проверки умножения на ноль и целочисленного деления

--- data/test_calculator.py
+++ data/test_calculator.py
@@ -1,6 +1,7 @@
 import unittest
 from simple_calculator import Calculator, fun
 from math import pow, sqrt
+from random import random
 
 
 class TestCalculator(unittest.TestCase):
@@ -19,9 +20,19 @@ class TestCalculator(unittest.TestCase):
         calc_value = self.calculator.value
         self.assertEqual(self.calculator.multiply(5, 2, 90).value, calc_value * 900)
 
+    def test_mul_zero(self):
+        self.calculator.value = 4
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.multiply(5, 2, 90, 0).value, 0)
+
     def test_divide(self):
         calc_value = self.calculator.value
         self.assertEqual(self.calculator.divide(2, 3).value, calc_value / 6)
+
+    def test_divide_int(self):
+        self.calculator.value = 20
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.divide(2, 4, integer_divide=True).value, calc_value // 8)
 
     def test_subtract(self):
         calc_value = self.calculator.value
@@ -34,6 +45,7 @@ class TestCalculator(unittest.TestCase):
     def test_root(self):
         calc_value = self.calculator.value
         self.assertEqual(self.calculator.root().value, sqrt(calc_value))
+
     # def test_file_read(self):
     #     with open('file.txt', 'r') as f:
     #         text = f.read()
